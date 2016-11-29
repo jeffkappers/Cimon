f0d736095fe1d4e35df6f0d4bb001d29ceca6882
Jefferson D Kappers <thisisnotjeffsemail@gmail.com>
2016-11-29 18:59:44 -0500

    Add files via upload

--- ViewController.swift
+++ ViewController.swift
@@ -1,37 +1,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    @IBAction func greenButtonPress(sender: UIButton) {
-        print("green")
-        let buttonValue = 0
-        myCimon.receivePlayersInput(buttonPressed: buttonValue, sequenceA: nil, sequenceB: nil)
-    }
-    @IBAction func redButtonPress(sender: UIButton) {
-        print("red")
-        let buttonValue = 1
-        myCimon.receivePlayersInput(buttonPressed: buttonValue, sequenceA: nil, sequenceB: nil)
-    }
-    @IBAction func yellowButtonPress(sender: UIButton) {
-        print("yellow")
-        let buttonValue = 2
-        myCimon.receivePlayersInput(buttonPressed: buttonValue, sequenceA: nil, sequenceB: nil)
-    }
-    @IBAction func blueButtonPress(sender: UIButton) {
-        print("blue")
-        let buttonValue = 3
-        myCimon.receivePlayersInput(buttonPressed: buttonValue, sequenceA: nil, sequenceB: nil)
-    }
-    
-    private var myCimon = Cimon()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        cimonGame()
+        aCimon.updateCimonsSequence()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func greenButtonPress(_ sender: AnyObject) {
+        print("The player press the Green button")
+        aCimon.updatePlayersSequenceThisTurn(input: "Green")
+    }
+    @IBAction func redButtonPress(_ sender: AnyObject) {
+        print("The player press the Red button")
+        aCimon.updatePlayersSequenceThisTurn(input: "Red")
+    }
+    @IBAction func yellowButtonPress(_ sender: AnyObject) {
+        print("The player press the Yellow button")
+        aCimon.updatePlayersSequenceThisTurn(input: "Yellow")
+    }
+    @IBAction func blueButtonPress(_ sender: AnyObject) {
+        print("The player press the Blue button")
+        aCimon.updatePlayersSequenceThisTurn(input: "Blue")
+    }
+        
+    var aCimon = Cimon()
+    
+    func cimonGame() {
+        while aCimon.isStillGoing == true {
+            print("Penis")
+        }
     }
 }
