f8c98095b2cdc104181bf870f991b256351bfb50
Jefferson D Kappers <thisisnotjeffsemail@gmail.com>
2016-11-29 18:59:27 -0500

    Add files via upload
    New version of Cimon, having trouble though when I want to go to the next turn how to go about doing that because the current setup is that the game is pretty much run through the updatePlayersSequenceThisTurn() function which is only called when a button is pressed. I'm not sure how to make it so that the cimonGame() function inside view controller class to control the game and allow for the input to be used or prohibited.

--- Cimon.swift
+++ Cimon.swift
@@ -2,68 +2,55 @@ import Foundation
 
 class Cimon {
     
+    var turnNumber = 0
+    var playersTurn = false
+    var cimonsTurn = true
+    var cimonsSequence = [String]()
+    var playersSequenceThisTurn = [String]()
+    var isStillGoing = true
     
-    fileprivate var isGameOver = false
-    fileprivate var isUsersTurn = true
-    
-    func protocol72 () -> String{
-        var gameOn = true
-        var playersTurn = false
-        while gameOn == true {
-            var thisGamesSequence = generateASequence()
-            while playersTurn == false {
-                thisGamesSequence = modifyASequence(currentSequence: thisGamesSequence)
-                for element in thisGamesSequence{ //testing to see if thisGameSequence is generated properly
-                    print(element)
-                }
-                playersTurn = true
-            }
-            while playersTurn == true {
-                var usersSequenceThisRound = generateASequence()
-                usersSequenceThisRound = receivePlayersInput(buttonPressed: nil, sequenceA: usersSequenceThisRound, sequenceB: thisGamesSequence)
-                if (usersSequenceThisRound.count == thisGamesSequence.count) {
-                    playersTurn = false
-                    let isGameStillOn = passOrFail(sequenceA: usersSequenceThisRound , sequenceB: thisGamesSequence)
-                    if (isGameStillOn == false) {
-                        gameOn = false
-                    }
-                }
-            }
+    func updatePlayersSequenceThisTurn(input : String) {
+        if playersSequenceThisTurn.count < cimonsSequence.count {
+            playersSequenceThisTurn.append(input)
+            print(playersSequenceThisTurn)
         }
-            return "You suck"
-    }
-    
-    func receivePlayersInput(buttonPressed: Int?, sequenceA: Array<Int>?, sequenceB: Array<Int>?) -> Array<Int> {
-        var sequenceC = sequenceA!
-        sequenceC.append(buttonPressed!)
-        for element in sequenceA! { //testing to see if button presses are properly appended to useresSequenceThisRound
-            print(element)
+        else {
+            isGameOver(playersSequenceThisTurn, cimonsSequence)
+            playersSequenceThisTurn.removeAll()
+            turnNumber += 1
+            updateCimonsSequence()
+            
         }
-        return sequenceC
-    }
-
-     private func generateASequence () -> Array<Int> {
-        var rand = 0
-        var currentSequence = [Int]()
-        rand = Int(arc4random_uniform(3))
-        currentSequence.append(rand)
-        return currentSequence
     }
     
-    private func modifyASequence (currentSequence : Array<Int>) -> Array<Int> {
-        var rand = 0
-        rand = Int(arc4random_uniform(3))
-        var myCurrentSequence = currentSequence
-        myCurrentSequence.append(rand)
-        return myCurrentSequence
-    }
-    
-    private func passOrFail(sequenceA: Array<Int>, sequenceB: Array<Int>) -> Bool {
-        for index in 0...sequenceA.count {
-            if (sequenceA[index] != sequenceB[index]){
-                return false
-            }
+    func updateCimonsSequence() {
+        let colorIndex = arc4random_uniform(4)
+        if colorIndex == 0 {
+            cimonsSequence.append("Green")
         }
-        return true
+        else if colorIndex == 1 {
+            cimonsSequence.append("Red")
+        }
+        else if colorIndex == 2 {
+            cimonsSequence.append("Yellow")
+        }
+        else {
+            cimonsSequence.append("Blue")
+        }
+        print("Cimon's current sequence is")
+        for colors in cimonsSequence {
+            print(colors)
+        }
+        cimonsTurn = false
+        playersTurn = true
+    }
+    
+    func isGameOver(_ : Array<String>, _ : Array<String>) {
+        if playersSequenceThisTurn == cimonsSequence {
+            print("Game is not over")
+        }
+        else {
+            print("Game is over")
+        }
     }
 }
