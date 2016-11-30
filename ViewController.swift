93d1c022912d073d1a1da434e335c9def104584c
Jefferson D Kappers <thisisnotjeffsemail@gmail.com>
2016-11-29 20:18:58 -0500

    Add files via upload
    I combined the functions for each color button pressed into one function that sends the title of button which is the color. Will have to adjust later because in the end the buttons won't have any text on them but for now it's fine, just trying to streamline the code.

--- ViewController.swift
+++ ViewController.swift
@@ -3,8 +3,8 @@ import UIKit
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        cimonGame()
         aCimon.updateCimonsSequence()
+        cimonGame()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -13,28 +13,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func greenButtonPress(_ sender: AnyObject) {
-        print("The player press the Green button")
-        aCimon.updatePlayersSequenceThisTurn(input: "Green")
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        print(sender.currentTitle)
+        aCimon.updatePlayersSequenceThisTurn(input: sender.currentTitle!)
     }
-    @IBAction func redButtonPress(_ sender: AnyObject) {
-        print("The player press the Red button")
-        aCimon.updatePlayersSequenceThisTurn(input: "Red")
-    }
-    @IBAction func yellowButtonPress(_ sender: AnyObject) {
-        print("The player press the Yellow button")
-        aCimon.updatePlayersSequenceThisTurn(input: "Yellow")
-    }
-    @IBAction func blueButtonPress(_ sender: AnyObject) {
-        print("The player press the Blue button")
-        aCimon.updatePlayersSequenceThisTurn(input: "Blue")
-    }
+    
         
     var aCimon = Cimon()
-    
+    //aCimon.updatePlayersSequenceThisTurn(input: "Blue")
     func cimonGame() {
-        while aCimon.isStillGoing == true {
-            print("Penis")
-        }
+        //while aCimon.isStillGoing == true {
+           // print("Penis")
+        //}
     }
 }
