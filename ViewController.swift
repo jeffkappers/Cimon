8fe0ecfd33801b1173807445f47ca8026d2f18a4
Jefferson D Kappers <thisisnotjeffsemail@gmail.com>
2017-03-12 14:09:22 -0400

    fixed overlapping button animations
    i was walking down main st last night and it dawned on me that to prevent the button animations from overlapping one another the function that performs the button animations would have to be called recursively in the completion: parameter of UIButton.animate. all that's left to do now is load a new view when the player loses the game which asks them if they'd like to play again.

--- ViewController.swift
+++ ViewController.swift
@@ -19,101 +19,140 @@ class ViewController: UIViewController {
     
 
     @IBAction func buttonPress(_ sender: UIButton) {
-        //let buttonTitle = sender.currentTitle
-        //print("\(buttonTitle) was pressed")
         var buttonValue: Int? = nil;
         if let buttonTitle = sender.currentTitle {
             switch buttonTitle {
             case "Green":
                 buttonValue = 0
                 let originalButtonColor = greenButton.backgroundColor
-                UIButton.animate(withDuration: 1, animations: {
-                    self.greenButton.backgroundColor = UIColor.white
-                    self.greenButton.backgroundColor = originalButtonColor
+                UIButton.animate(
+                    withDuration: 1,
+                    animations: {
+                        self.greenButton.backgroundColor = UIColor.white
+                        self.greenButton.backgroundColor = originalButtonColor},
+                    completion: { (value: Bool) in
+                        self.isGameOver(buttonValue: buttonValue!)
                 })
             case "Red":
                 buttonValue = 1
                 let originalButtonColor = redButton.backgroundColor
-                UIButton.animate(withDuration: 1, animations: {
-                    self.redButton.backgroundColor = UIColor.white
-                    self.redButton.backgroundColor = originalButtonColor
+                UIButton.animate(
+                    withDuration: 1,
+                    animations: {
+                        self.redButton.backgroundColor = UIColor.white
+                        self.redButton.backgroundColor = originalButtonColor},
+                    completion: { (value: Bool) in
+                        self.isGameOver(buttonValue: buttonValue!)
                 })
             case "Yellow":
                 buttonValue = 2
                 let originalButtonColor = yellowButton.backgroundColor
-                UIButton.animate(withDuration: 1, animations: {
-                    self.yellowButton.backgroundColor = UIColor.white
-                    self.yellowButton.backgroundColor = originalButtonColor
+                UIButton.animate(
+                    withDuration: 1,
+                    animations: {
+                        self.yellowButton.backgroundColor = UIColor.white
+                        self.yellowButton.backgroundColor = originalButtonColor},
+                    completion: { (value: Bool) in
+                        self.isGameOver(buttonValue: buttonValue!)
                 })
             case "Blue":
                 buttonValue = 3
                 let originalButtonColor = blueButton.backgroundColor
                 blueButton.backgroundColor = UIColor.white
-                UIButton.animate(withDuration: 1, animations: {
-                    self.blueButton.backgroundColor = UIColor.white
-                    self.blueButton.backgroundColor = originalButtonColor
+                UIButton.animate(
+                    withDuration: 1,
+                    animations: {
+                        self.blueButton.backgroundColor = UIColor.white
+                        self.blueButton.backgroundColor = originalButtonColor},
+                    completion: { (value: Bool) in
+                        self.isGameOver(buttonValue: buttonValue!)
                 })
             default:
                 buttonValue = 0
             }
 
         }
-        aCimonGame.updatePlayersSequence(playersInput: buttonValue!)
-        if aCimonGame.gameOver == true {
-            print("you lost")
-        }
-        shouldDisplayCimonsSequence()
+        
+        
     }
     
     private var aCimonGame = CimonGame()
-    var index = 0;
+    var cimonsIndex = 0;
     
+    func isGameOver(buttonValue: Int) {
+        aCimonGame.updatePlayersSequence(playersInput: buttonValue)
+        if aCimonGame.gameOver == true {
+            print("you lost")
+        }
+        else {
+            shouldDisplayCimonsSequence()
+        }
+    }
     
     func shouldDisplayCimonsSequence(){
         if aCimonGame.playerFinishedThisSequence == true {
-            print("Cimon's sequence count is \(aCimonGame.cimonsSequence.count)")
-            while index <= aCimonGame.cimonsSequence.count-1 {
-                print("index's value is \(index)")
-                switch aCimonGame.cimonsSequence[index]{
-                case 0:
-                    let originalButtonColor = self.greenButton.backgroundColor
-                    UIButton.animate(withDuration: 2, delay: 1,
+            displayCimonsSequence()
+        }
+    }
+    
+    func displayCimonsSequence() {
+        if cimonsIndex <= aCimonGame.cimonsSequence.count-1 {
+            switch aCimonGame.cimonsSequence[cimonsIndex]{
+            case 0:
+                cimonsIndex += 1
+                let originalButtonColor = self.greenButton.backgroundColor
+                UIButton.animate(
+                    withDuration: 1,
                     animations: {
                         self.greenButton.backgroundColor = UIColor.white
-                        self.greenButton.backgroundColor = originalButtonColor
-                    },
+                        self.greenButton.backgroundColor = originalButtonColor},
                     completion: {
-                            (value: Bool) in
-                    })
-                case 1:
-                    let originalButtonColor = self.redButton.backgroundColor
-                    UIButton.animate(withDuration: 2,
+                        (value: Bool) in
+                        self.displayCimonsSequence()
+                })
+            case 1:
+                cimonsIndex += 1
+                let originalButtonColor = self.redButton.backgroundColor
+                UIButton.animate(
+                    withDuration: 1,
                     animations: {
                         self.redButton.backgroundColor = UIColor.white
-                        self.redButton.backgroundColor = originalButtonColor
-                    })
-                case 2:
-                    let originalButtonColor = self.yellowButton.backgroundColor
-                    UIButton.animate(withDuration: 2,
+                        self.redButton.backgroundColor = originalButtonColor},
+                    completion: {
+                        (value: Bool) in
+                        self.displayCimonsSequence()
+                })
+            case 2:
+                cimonsIndex += 1
+                let originalButtonColor = self.yellowButton.backgroundColor
+                UIButton.animate(
+                    withDuration: 1,
                     animations: {
                         self.yellowButton.backgroundColor = UIColor.white
-                        self.yellowButton.backgroundColor = originalButtonColor
-                    })
-                case 3:
-                    let originalButtonColor = self.blueButton.backgroundColor
-                    UIButton.animate(withDuration: 2,
+                        self.yellowButton.backgroundColor = originalButtonColor},
+                    completion: {
+                        (value: Bool) in
+                        self.displayCimonsSequence()
+                })
+            case 3:
+                cimonsIndex += 1
+                let originalButtonColor = self.blueButton.backgroundColor
+                UIButton.animate(
+                    withDuration: 1,
                     animations: {
                         self.blueButton.backgroundColor = UIColor.white
-                        self.blueButton.backgroundColor = originalButtonColor
-                    })
-                default:
-                    print("how'd i get here?")
-                }
-                index += 1
-                
+                        self.blueButton.backgroundColor = originalButtonColor},
+                    completion: {
+                        (value: Bool) in
+                        self.displayCimonsSequence()
+                })
+            default:
+                print("how'd i get here?")
             }
         }
-
+        else {
+            self.cimonsIndex = 0
+            }
     }
     
     @IBOutlet weak var greenButton: UIButton!
